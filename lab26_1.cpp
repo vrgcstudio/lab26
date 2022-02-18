26aa0c53e492fa12d0f1ff3c249bfea9b7b4bfdc
Rapepol <vrgcstudio@gmail.com>
2022-02-18 20:53:20 +0700

    din

--- lab26_1.cpp
+++ lab26_1.cpp
@@ -63,4 +63,22 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int idx){
+	Node *n = new Node;
+	if(idx == 0){
+		n = root;
+		root = root->next;
+		delete n;
+	}
+	else{
+		Node *current = new Node;
+		current = root;
+		for (int i=0;i < idx-1;i++){
+			current = current->next;
+		}
+		n = current->next;
+		current->next = current->next->next;
+		delete n;
+	}
+	size--;
+}
--- lab26_3.cpp
+++ lab26_3.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
-//Write function count() here.
+int count(int data[100],int size){
+	set <int> mySet;
+	for (int i=0;i<size;i++)  mySet.insert(data[i]);
+	return mySet.size();
+}
+
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
